f09df5bcaf190296feae01bd41324f701c7db4a7
alex <miot-alexandre@bbox.fr>
2016-09-06 23:21:03 +0200

    client

--- pytt/client/client.js
+++ pytt/client/client.js
@@ -1,50 +1,43 @@
-var margin = { top: 20, right: 20, bottom: 30, left: 40 };
-var width = 800 - margin.left - margin.right;
-var height = 600 - margin.top - margin.bottom;
-var data = new Array();
-
-var xScale = d3.scaleLinear()
-    .domain([90, 110])  // domaine d'entrée
-    .range([0, width]);  // domaine de sortie
-
-
-var yScale = d3.scaleLinear()
-    .range([90, 110])
-    .domain([height, 0]);
-
-var chart = d3.select('#chart').append('svg')
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
     // .append("g")
     // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-function colScale(i) {
-    return d3.schemeCategory20(i % 20);
-}
-
 function parseData(dta_str) {
-    var data = dta_str.replace(/[\[\] ]/g, "").trim().split(',').map(Number);
-    return data;
+    var d = dta_str.trim().split('|');
+    var id = Number(d[0]);
+    var cat = d[1].split('.')[1];
+    var num_data = d[2].replace(/[\[\] ]/g, "").trim().split(',').map(Number);
+    return {'id': id, 'cat': cat, 'data': num_data};
 }
 
 
 function wsUpdate(event) {
-    numData = parseData(event.data);
-    console.log("new -> " + numData);
-    data.push(numData);
-    redraw(data);
+    par_data = parseData(event.data);
+    switch(par_data.cat) {
+    case "PL":
+        console.log("pl " + par_data.data);
+        break;
+    case "STREAM":
+        console.log("data " + par_data.data);
+        data_stack.push(par_data.data);
+        redraw(data_stack, chart_data);
+        break;
+    case "POSITION":
+        console.log("position " + par_data.data);
+        break;
+    default:
+        console.log("got other cat");
+}
 }
 
-function redraw(d) {
+function redraw(d, chart) {
     console.log('red')
     var circles = chart.selectAll('circle')
         .attr("class", "chart")
         .data(d);
 
     circles.enter().append('circle')
-        .attr("cx", function(d) { return xScale(d.x); })
-        .attr("cy", function(d) { return yScale(d.y); })
+        .attr("cx", function(d) { return xScale(d[0]); })
+        .attr("cy", function(d) { return yScale(d[1]); })
         .attr("r", 1);
 
     // circles.exit()
@@ -60,26 +53,18 @@ function redraw(d) {
         .attr("fill", function(d, i) { return colScale(i); })
         .transition()
         .duration(750)
-        .attr("cx", function(d) { return xScale(d.x); })
-        .attr("cy", function(d) { return yScale(d.y); })
+        .attr("cx", function(d) { return xScale(d[0]); })
+        .attr("cy", function(d) { return yScale(d[1]); })
         .attr("r", function(d) { return 2; });
 }
 
 function startWs() {
     console.log("starting client");
-    var delay = 1;
-    var ws = null;
-    function f() {
-        try {
-            w = new WebSocket("ws://127.0.0.1:5678/");
-        } catch (err) {
-            console.log(err);
-            return null;
-        }
-        return w;
+    try {
+        w = new WebSocket("ws://127.0.0.1:5678/");
+    } catch (err) {
+        console.log(err);
+        return null;
     }
-    ws = f();
-    if (ws != null) {
-        ws.onmessage = wsUpdate;
-    }
+    w.onmessage = wsUpdate;
 }
--- pytt/client/graph.js
+++ pytt/client/graph.js
@@ -1,7 +1,11 @@
 var margin = { top: 20, right: 20, bottom: 30, left: 40 };
-var width = 800 - margin.left - margin.right;
-var height = 600 - margin.top - margin.bottom;
-var data = new Array();
+var width = 300 - margin.left - margin.right;
+var height = 200 - margin.top - margin.bottom;
+
+// data stacks
+var data_stack = new Array();
+var pl_stack = new Array();
+var position_stack = new Array();
 
 var xScale = d3.scaleLinear()
     .domain([90, 110])  // domaine d'entrée
@@ -9,11 +13,17 @@ var xScale = d3.scaleLinear()
 
 
 var yScale = d3.scaleLinear()
-    .range([90, 110])
+    .range([100, 120])
     .domain([height, 0]);
 
-var chart = d3.select("body").select("div#chart").append("svg")
+function colScale(i) {
+    return d3.schemeCategory20[i % 20];
+}
+
+var charts = d3.select("body").selectAll("div.chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom);
 
-console.log(chart);
+var chart_data = d3.select("body").select("div#data").select("svg");
+var chart_pl = d3.select("body").select("div#pl").select("svg");
+var chart_position = d3.select("body").select("div#position").select("svg");
